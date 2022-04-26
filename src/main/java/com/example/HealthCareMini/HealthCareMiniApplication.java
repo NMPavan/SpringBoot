5eb5d5e808672d7886a0c5b694157767dd3f7533
Manikyapavan <manikyapavan123@gmail.com>
2022-04-26 08:41:13 +0530

    intial commit

---
+++ src/main/java/com/example/HealthCareMini/HealthCareMiniApplication.java
@@ -0,0 +1,13 @@
+package com.example.HealthCareMini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareMiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareMiniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HealthCareMini/HealthCareMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HealthCareMini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareMiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
